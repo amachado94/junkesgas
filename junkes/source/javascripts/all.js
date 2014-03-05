3f6b38e84e0c01e655c1f3fb65ce043742755ea8
Alisson Andrey Machado <alisson.machado@cenize.com>
2014-03-05 20:29:04 -0300

    Alterações em imagens e código
    - Transferência de imagens;
    - Novas definições no CSS;
    - Novo conteúdo adicionado em Contato, Index.

--- junkes/source/javascripts/all.js
+++ junkes/source/javascripts/all.js
@@ -1,1 +1,8 @@
-//= require_tree .
+script link_to="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
+script
+	$(document).ready(function(){
+  		$("#flip").click(function(){
+    		$("#panel").slideToggle("slow");
+  		});
+	});
+ 
