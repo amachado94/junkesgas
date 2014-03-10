5e6b03148ebee0094514238b1fb5a20fff2a043f
Alisson Andrey Machado <alisson.machado@cenize.com>
2014-03-10 20:50:14 -0300

    Novas alterações no Site

--- junkes/source/javascripts/all.js
+++ junkes/source/javascripts/all.js
@@ -1,8 +1,10 @@
-script link_to="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
-script
-	$(document).ready(function(){
-  		$("#flip").click(function(){
-    		$("#panel").slideToggle("slow");
-  		});
-	});
+//= require jquery
+
+src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
+
+$(document).ready(function(){
+  	$("#flip").click(function(){
+    	$("#panel").slideToggle("slow");
+  	});
+});
  
